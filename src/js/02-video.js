358972a791097b92be9a3700f7aa2be619077ff0
OlegGalepa <113933558+OlegGalepa@users.noreply.github.com>
2023-03-12 22:43:13 +0200

    task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
+// Зберігай час відтворення у локальне сховище. Нехай ключем для сховища буде рядок "videoplayer-current-time".
+// Під час перезавантаження сторінки скористайся методом setCurrentTime() з метою відновлення відтворення зі збереженої позиції.
+// Додай до проекту бібліотеку lodash.throttle і зроби так, щоб час відтворення оновлювався у сховищі не частіше, ніж раз на секунду.
 
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const LOCAL_KEY = 'videoplayer-current-time';
+
+player.setCurrentTime(localStorage.getItem(LOCAL_KEY)).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
+
+player.on('play', function (time) {
+	
+});
+
+player.on('timeupdate', throttle((time) => {
+    localStorage.setItem(LOCAL_KEY, time.seconds);
+}));
